e1f42374b050f981ca1304adfe213882314e5e6c
RhysWeb <Rhys25780@hotmail.com>
2022-07-05 21:28:23 +0100

    first init

---
+++ src/pages/GasConvert/GasConvert.tsx
@@ -0,0 +1,25 @@
+export default function GasConvert() {
+	return (
+		<div>
+			<h1>Gas Convert</h1>
+			<p>
+				<input
+					type="text"
+					placeholder="Enter the amount of gas you want to convert"
+				/>
+			</p>
+			<p>
+				<input
+					type="text"
+					placeholder="Enter the unit of gas you want to convert"
+				/>
+			</p>
+			<p>
+				<input
+					type="text"
+					placeholder="Enter the unit of gas you want to convert to"
+				/>
+			</p>
+		</div>
+	);
+}
---
+++ src/pages/GasConvert/index.ts
@@ -0,0 +1,2 @@
+export { default } from './GasConvert';
+//this file is called a 'barrel' and it allows me to give my page a file name other than index.js whilst still putting it in a folder.
